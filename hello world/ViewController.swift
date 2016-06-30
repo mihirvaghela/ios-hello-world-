c630d7e60121f1b81d0bf7e88e555adccc797949
Mihir Vaghela <mihirvaghela@mihirs-macbook-pro.local>
2016-06-30 04:42:12 -0400

    Initial Commit

---
+++ hello world/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hello world
+//
+//  Created by Mihir Vaghela on 2016-06-30.
+//  Copyright © 2016 Mihir Vaghela. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
